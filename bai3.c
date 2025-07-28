65bf14ad93c65521228b5f159d226864584b4ea8
ngyenngoc <ng.yenngoc.work@gmail.com>
2025-07-28 12:09:26 +0700

    bai3.c

--- bai3.c
+++ bai3.c
@@ -1,1 +1,12 @@
-gdbxgjds 
+#include <stdio.h>
+int main(){
+	char str []="agfdnfhj";
+	strupr(str);
+	printf("chu in hoa la:%s\n",str);
+	strlwr(str);
+	printf("chu in thuong la:%s\n",str);
+	int dodai=strlen(str);
+	printf("do dai chuoi la:%d\n",dodai);
+		return 0;
+		}
+
--- cgj.c
+++ cgj.c
@@ -2,7 +2,7 @@
 
 int main() {
     int a[] = {2, 3, 5, 1, 7};
-    int n = sizeof(a) / sizeof(a[0]); // Tính s? ph?n t? trong m?ng
+    int n = sizeof(a) / sizeof(a[0]); 
 
     printf("Cac so chan la: ");
     for (int i = 0; i < n; i++) {
